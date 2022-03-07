f0b65d303a5b44462cefeebf68f5988bcbd52de0
Damien Marest <damien.marest@gmail.com>
2022-03-07 21:04:24 +0100

    test: add ngmocks and initiate unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,56 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { MatToolbar } from '@angular/material/toolbar';
-import { MockComponent, ngMocks } from 'ng-mocks';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import {
+  MockBuilder,
+  MockRender,
+  MockedComponentFixture,
+  ngMocks,
+} from 'ng-mocks';
 
 import { AppComponent } from './app.component';
+import { AppModule } from './app.module';
+import * as breakpointSelectors from './core/stores/breakpoint/breakpoint.selectors';
 
 describe('AppComponent', () => {
-  let fixture: ComponentFixture<AppComponent>;
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [MockComponent(MatToolbar), AppComponent],
-    }).compileComponents();
-    fixture = TestBed.createComponent(AppComponent);
+  let fixture: MockedComponentFixture<AppComponent>;
+  let store: MockStore;
+  beforeEach(() =>
+    MockBuilder(AppComponent, AppModule).provide(
+      provideMockStore({
+        selectors: [
+          {
+            selector: breakpointSelectors.isDesktop,
+            value: true,
+          },
+        ],
+      })
+    )
+  );
+
+  beforeEach(() => {
+    fixture = MockRender(AppComponent);
+    store = TestBed.inject(MockStore);
     fixture.detectChanges();
   });
 
-  it(`should have as title '/ME'`, () => {
-    expect(ngMocks.find(MatToolbar).nativeElement.innerHTML).toContain('/ME');
-  });
+  describe('Header', () => {
+    describe('Desktop mode', () => {
+      fit(`should not have a toolbar as it is desktop mode'`, () => {
+        expect(ngMocks.find('mat-toolbar', null)).toBeNull();
+      });
+    });
 
-  it('should render header with name', () => {
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('main H1')?.textContent).toContain(
-      'Damien Marest'
-    );
+    describe('Mobile mode', () => {
+      beforeEach(() => {
+        breakpointSelectors.isDesktop.setResult(false);
+        store.refreshState();
+        fixture.detectChanges();
+      });
+
+      fit(`should have a toolbar as it is mobile mode'`, () => {
+        expect(ngMocks.find('mat-toolbar', null)).not.toBeNull();
+      });
+    });
   });
 });
